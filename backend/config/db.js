630e15e42a4bc320ff59b7c3667a099b48cfbfda
FalguniJain <115800323+FalguniJain@users.noreply.github.com>
2025-10-28 23:51:29 +0530

    db config updation
    Removed redundant assignment of mongoURI.

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,6 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     // Read the URI from an environment variable
-    const mongoURI = process.env.MONGODB_URI;
     await mongoose.connect(`${process.env.MONGODB_URI}/food_delivery`)
         .then(() => console.log("DB connected"));
 }
