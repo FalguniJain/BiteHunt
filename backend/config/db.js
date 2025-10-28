ff4640b2eb4e70d21521726e2e404097f06f743d
FalguniJain <falgunijain2812@gmail.com>
2025-10-28 16:47:33 +0530

    Fixed MongoDB connection to use environment variable

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,9 @@
 import mongoose from "mongoose";
 
-export  const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://falgunijain2812_db_user:123456578@cluster0.ek3irvp.mongodb.net/food_delivery').then(()=>console.log("DB connected"));   
+
+export const connectDB = async () => {
+    // Read the URI from an environment variable
+    const mongoURI = process.env.MONGODB_URI;
+    await mongoose.connect(`${process.env.MONGODB_URI}/food_delivery`)
+        .then(() => console.log("DB connected"));
 }
