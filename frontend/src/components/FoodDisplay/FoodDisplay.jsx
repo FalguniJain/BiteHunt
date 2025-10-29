1cd69803149589dbd30b9f542e487e72f81c8316
FalguniJain <115800323+FalguniJain@users.noreply.github.com>
2025-10-29 12:25:41 +0530

    Update FoodDisplay.jsx

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react'
 import './FoodDisplay.css'
 import { StoreContext } from '../../context/StoreContext'
-import FoodItem from '../foodItem/foodItem';
+import FoodItem from '../FoodItem/FoodItem';
 
 // eslint-disable-next-line no-unused-vars
 const FoodDisplay = ({category}) => {
